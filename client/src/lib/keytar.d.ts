50c9452e40b3a08a33f70df1f6760a417e624f9d
hijecj <1491903593@qq.com>
2026-08-03 13:59:09 +0800

    Translate comments: keytar.d.ts

--- client/src/lib/keytar.d.ts
+++ client/src/lib/keytar.d.ts
@@ -1,24 +1,24 @@
-// Definitions by: Milan Burda <https://github.com/miniak>, Brendan Forster <https://github.com/shiftkey>, Hari Juturu <https://github.com/juturu>
-// Adapted from DefinitelyTyped: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/keytar/index.d.ts
+// 定义者：Milan Burda <https://github.com/miniak>、Brendan Forster <https://github.com/shiftkey>、Hari Juturu <https://github.com/juturu>
+// 改编自 DefinitelyTyped：https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/keytar/index.d.ts
 
 /**
- * Get the stored password for the service and account.
+ * 获取服务和账户的已存储密码。
  *
- * @param service The string service name.
- * @param account The string account name.
+ * @param service 字符串服务名。
+ * @param account 字符串账户名。
  *
- * @returns A promise for the password string.
+ * @returns 密码字符串的 Promise。
  */
 export declare function getPassword(service: string, account: string): Promise<string | null>
 
 /**
- * Add the password for the service and account to the keychain.
+ * 把服务和账户的密码添加到钥匙串。
  *
- * @param service The string service name.
- * @param account The string account name.
- * @param password The string password.
+ * @param service 字符串服务名。
+ * @param account 字符串账户名。
+ * @param password 字符串密码。
  *
- * @returns A promise for the set password completion.
+ * @returns 设置密码完成的 Promise。
  */
 export declare function setPassword(
   service: string,
@@ -27,30 +27,30 @@ export declare function setPassword(
 ): Promise<void>
 
 /**
- * Delete the stored password for the service and account.
+ * 删除服务和账户的已存储密码。
  *
- * @param service The string service name.
- * @param account The string account name.
+ * @param service 字符串服务名。
+ * @param account 字符串账户名。
  *
- * @returns A promise for the deletion status. True on success.
+ * @returns 删除状态的 Promise。成功时为 true。
  */
 export declare function deletePassword(service: string, account: string): Promise<boolean>
 
 /**
- * Find a password for the service in the keychain.
+ * 在钥匙串中查找服务的密码。
  *
- * @param service The string service name.
+ * @param service 字符串服务名。
  *
- * @returns A promise for the password string.
+ * @returns 密码字符串的 Promise。
  */
 export declare function findPassword(service: string): Promise<string | null>
 
 /**
- * Find all accounts and passwords for `service` in the keychain.
+ * 在钥匙串中查找 `service` 的所有账户和密码。
  *
- * @param service The string service name.
+ * @param service 字符串服务名。
  *
- * @returns A promise for the array of found credentials.
+ * @returns 找到的凭证数组的 Promise。
  */
 export declare function findCredentials(
   service: string
